d9a893c093e75ab3e10ea5f3e4e02dea794dc5ee
Pierre Ribeiro <pierre.ribeiro-gubertech@amyris.com>
2025-12-22 19:59:06 -0300

    Reestruturação da árvore de diretórios

--- procedures/original/dbo.MaterialToTransition.sql
+++
@@ -1,8 +0,0 @@
-/****** Object:  StoredProcedure [dbo].[MaterialToTransition]    Script Date: 21/10/2025 16:05:19 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE PROCEDURE [dbo].[MaterialToTransition] @MaterialUid VARCHAR(50), @TransitionUid VARCHAR(50) AS
-	INSERT INTO material_transition (material_id, transition_id) VALUES (@MaterialUid, @TransitionUid)
-GO
--- procedures/original/dbo.TransitionToMaterial.sql
+++
@@ -1,8 +0,0 @@
-/****** Object:  StoredProcedure [dbo].[TransitionToMaterial]    Script Date: 21/10/2025 16:05:19 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE PROCEDURE [dbo].[TransitionToMaterial] @TransitionUid VARCHAR(50), @MaterialUid VARCHAR(50) AS
-	INSERT INTO transition_material (material_id, transition_id) VALUES (@MaterialUid, @TransitionUid)
-GO
